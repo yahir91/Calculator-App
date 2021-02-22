6fb636dde6809d145314215466063590fc7e69dd
Yahir Cardona <yahir.cardona@gmail.com>
2021-02-22 15:36:58 -0600

    Button panel and display files added

--- src/components/App.js
+++ src/components/App.js
@@ -1,42 +1,14 @@
- import React, { PureComponent } from 'react';
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 
-class App extends PureComponent {
-  render() {
-    return (
+const App = () => {
+  return ( 
       <>
         <Display />
         <ButtonPanel />
       </>
-    );
-  }
-}
-
-export default App;
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit
-          {' '}
-          <code>src/App.js</code>
-          {' '}
-          and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   );
 }
 
 export default App;
