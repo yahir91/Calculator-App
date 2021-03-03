d5608f2c8c885bbe3b1e4fe9dbe46bee5b6f6952
Yahir Cardona <yahir.cardona@gmail.com>
2021-03-03 12:55:20 -0600

    operate logic added

---
+++ src/logic/calculate.js
@@ -0,0 +1,20 @@
+
+import operate from './operate';
+
+const calculate = (data, buttonName) => {
+  const { total, next, operation } = data;
+  
+  switch (buttonName) {
+      case "AC":
+          total = "";
+          next = ""
+          operation = ""
+          break;
+      case "=":
+          
+  }
+
+
+
+}
+export default calculate;
---
+++ src/logic/operate.js
@@ -0,0 +1,31 @@
+
+import Big from 'big.js';
+
+const operate = (numberOne, numberTwo, operation) => {
+  const firstNumber = Big(numberOne);
+  const secondNumber = Big(numberTwo);
+
+  switch (operation) {
+    case '+':
+      return firstNumber + secondNumber;
+    case '-':
+      return firstNumber - secondNumber;
+    case 'X':
+      return firstNumber * secondNumber;
+    case '÷':
+      if (secondNumber === '0') {
+        return '0';
+      }
+      return firstNumber / secondNumber;
+    case '%':
+      return firstNumber * Big(0.01);
+    case '+/-':
+      return firstNumber * Big(-1);
+    default:
+      break;
+  }
+
+  return firstOperand;
+};
+
+export default operate;
