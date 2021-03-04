14e5d7439e1834fa1b92f5bb6d2338ae12839fea
Yahir Cardona <yahir.cardona@gmail.com>
2021-03-04 11:38:10 -0600

    App component converted to stateful to pass calculate logic

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
+import calculate from '../logic/calculate'
 
-const App = () => (
-  <>
-    <Display />
-    <ButtonPanel results="0" />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null
+    }
+  }
+
+  handleClick(buttonName) {
+    let calcData = this.state
+    let newData = calculate(calcData, buttonName)
+    const {total, next, operation} = newData
+    this.setState(total, next, operation)
+  }
+
+  render() {  
+    const {total, next} = this.state
+    return (
+      <div>
+        < Display display={next} />
+        < ButtonPanel onClick={()=> this.handleClick(buttonName)} />
+      </div>
+    )
+  }
+};
 
 export default App;
